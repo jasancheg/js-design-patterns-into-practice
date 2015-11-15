f1fc26be8294bbb2e8dab3bb05da48be257b4565
Jose Antonio Sanchez <jasancheg@gmail.com>
2015-11-15 13:40:39 -0600

    Chain of responsability pattern (cor)

--- js/cor/handlers/email.js
+++ js/cor/handlers/email.js
@@ -1,14 +1,24 @@
 define(function (require) {
     'use strict';
 
+    // get the base Handler
     var Handler = require('cor/handler'),
         emailHandler;
 
+    /**
+     * create a new email handler by create an instanse of the base `Handler` class
+     * @type {Handler}
+     */
     emailHandler = new Handler('email', handleEmail, null);
 
+    /**
+     * [handleEmail description]
+     * @param  {[string]} email [description]
+     */
     function handleEmail(email) {
         console.log('Email sent to', email.recipient, 'message: ', email.message);
     }
 
+    // return the email handler
     return emailHandler;
 });
