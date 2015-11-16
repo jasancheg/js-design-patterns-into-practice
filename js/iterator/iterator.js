f3e88586771ac9c9ab25b48381b559c7c501dcd4
Jose Antonio Sanchez <jasancheg@gmail.com>
2015-11-15 20:21:55 -0600

    Iterator pattern

--- js/iterator/iterator.js
+++ js/iterator/iterator.js
@@ -1,42 +1,93 @@
 define(function () {
     'use strict';
 
+    /**
+     * Define a constructor
+     * @param {[array]} collection [a collection of items]
+     */
     var Iterator = function (collection) {
+        // make own property of the constructor
         this.collection = collection;
+        // keep track of which item have been processed
         this.index = 0;
     };
 
+    /**
+     * All the methods for the constructor
+     * @type {Object}
+     */
     Iterator.prototype = {
+        // set the constructor property of the object that is going to be used
+        // for the prototype back to the iterator constructor, If we don't do this,
+        // the constructor will point to the object constructor instead
         constructor: Iterator,
+        
+        // iterators interface
+        
+        /**
+         * core of the iterator
+         * @return {object} [next item in the array using the index proxy of the iterator]
+         */
         next: function () {
+            // increment the index when the element is returned
+            // don't woory about coding defensively here because if we try to access 
+            // an array item that doesn't exist it would just return undefined
             return this.collection[this.index++];
         },
+
+        /**
+         * returns true once all the items in the collection have been traversed
+         * @return {Boolean} [description]
+         */
         isDone: function () {
             return this.index === this.collection.length;
         },
+        
+        /**
+         * sets the index property of the iterator back to 0
+         * @return {[Int]} [reset in case we want to traverse the collection again]
+         */
         reset: function () {
             this.index = 0;
             return this;
         },
+
+        /**
+         * Will return a specified number of items
+         * @param  {[Int]} numberOfItems  [number of items that we want to take]
+         * @return {[array]}              [specified number of items]
+         */
         take: function (numberOfItems) {
             var newIndex = this.index + numberOfItems,
+                // create new array using slice method, is safer to use it
+                // just in case the iterator's collection is a string
                 newArr = Array.prototype.slice.call(this.collection, this.index, newIndex);
 
+            // update the index accordingly once we've taken the items
             this.index = newIndex;
             return newArr;
         }
     };
 
     return {
+        /**
+         * build method is another implementation of the factory pattern
+         * @param  {[object]} collection [description]
+         * @return {[type]}              [description]
+         */
         build: function (collection) {
+
+            // just useful when a collection is passe
             var keys = Object.keys(collection),
                 tempArray = [],
                 prop;
 
+            // when number is passed convert it to a string and the iterator will be able to handle it
             if (typeof collection === 'number') {
                 collection = collection.toString();
             }
 
+            // when a object is passed extract the values from the object and construct a new array from these
             if (keys.length) {
                 for (prop in collection) {
                     tempArray.push(collection[prop]);
@@ -44,6 +95,7 @@ define(function () {
                 collection = tempArray;
             }
 
+            // try to initialize the iterator
             if (collection.length) {
                 return new Iterator(collection);
             } else {
--- js/iterator/readme.md
+++ js/iterator/readme.md
@@ -2,12 +2,20 @@
 
 > The Iterator Pattern
 
-JavaScript has some iteration built-in—for and for-in loops for example—but to use these, we need to know up-front what type of object we want to iterate. In this lesson, let's use the Iterator pattern to build an asynchronous iterator for any type of object.
+JavaScript has some iteration built-in—for and for-in loops for example—but to use these, we need to know up-front what type of object we want to iterate. This sample, shows how to use the Iterator pattern to build an asynchronous iterator for any type of object.
 
+There are a number of array methods tha we can use to iterate arrays and we have the for in loop for objects. But the iteration pattern is slightly different in that it doesn't have to happen in a synchronous loop, we choose when to extract the next item from the collection and the iteration itself keeps track of which items have been processed. The purpose of an iterator is to provide access to the items in a collection contiguously while keeping track of which items have been accessed. Usually,an iterator will provide a method called next which will return the next item in the collection. 
+
+In some languages, an iterator will provide other methods like hasNext, isDone, first, and reset. Javascript has iterators as a core part of the language in ES7, so iterators ara coming to a browser near you at some point in the future. Until is widely implemented, however, we'll have to ceate our own.
 
 **Features**
 
-* Web pages and UIs can built of independent modules
+* JavaScript has iteration built into the language
+* An iterator doesn't run in a synchronous loop
+* The iterator keeps track of which items have been processed
+* Access to the items is contiguous
+* Usually provides a next() method
+* Sometimes provides hasNext(), isDone(), first() and reset()
 
 
 **Summary**
