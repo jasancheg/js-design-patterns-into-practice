254dc0a4dadfd7bf154d80a0ad2b7b120cc6abbc
Jose Antonio Sanchez <jasancheg@gmail.com>
2015-11-15 18:17:54 -0600

    Iterator pattern

---
+++ js/iterator/readme.md
@@ -0,0 +1,15 @@
+# JS Design patterns into practice
+
+> The Iterator Pattern
+
+JavaScript has some iteration built-in—for and for-in loops for example—but to use these, we need to know up-front what type of object we want to iterate. In this lesson, let's use the Iterator pattern to build an asynchronous iterator for any type of object.
+
+
+**Features**
+
+* Web pages and UIs can built of independent modules
+
+
+**Summary**
+
+* 
--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
  * Main module
  */
 require(
-    ['factory/init', 'pubsub/init', 'observer/init', 'strategy/init', 'cor/init', 'mediator/init'],
-    function (factory, pubsub, observer, strategy, cor, mediator) {
+    ['factory/init', 'pubsub/init', 'observer/init', 'strategy/init', 'cor/init', 'mediator/init'
+    'iterator/init'],
+    function (factory, pubsub, observer, strategy, cor, mediator, iterator) {
         'use strict';
 
+        // allow to run specific examples by name
         var examplesList = {
             factory: factory,
             pubsub: pubsub,
@@ -15,10 +17,15 @@ require(
             mediator: mediator
         };
 
+        /**
+         * [runExample description]
+         * @param  {[string]} name [Pattern name]
+         * @return {[false]}       [if no name is provided or bad name stops the excecution]
+         */
         window.runExample = function (name) {
-            if(!name){
+            if(!name || !examplesList[name]){
                 console.group();
-                console.warn('you should provide a pattern name');
+                console.warn('you should provide a valid pattern name');
                 console.info('available pattern samples:');
                 console.log('  Creational Design Patterns: ', ['factory']);
                 console.log('  Behavioural Patterns: ', ['pubsub','strategy', 'observer', 'cor', 'mediator', 'iterator']);
