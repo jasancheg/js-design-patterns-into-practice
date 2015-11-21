71aef80bab262f91242560a96b446d9817bbd937
Jose Antonio Sanchez <jasancheg@gmail.com>
2015-11-21 10:29:29 -0600

    Decorator pattern

--- js/decorator/user.js
+++ js/decorator/user.js
@@ -1,6 +1,12 @@
 define(function () {
     'use strict';
 
+    /**
+     * Very simple constructor 
+     * User obejects have a getPermissions method wich is used to
+     * return a simple string that details their level of access
+     * @param {[string]} id [User instance id]
+     */
     var User = function (id) {
         this.id = id;
         this.getPermissions = function () {
@@ -8,9 +14,16 @@ define(function () {
         }
     };
 
+    /**
+     * Use to add new decorations
+     * This method simply overwrides the Object 
+     * `getPermissions` method with its own copy of the method  
+     * @param  {[function]} decorator [new decorator]
+     */
     User.prototype.decoratePermissions = function (decorator) {
         this.getPermissions = decorator.getPermissions;
     };
 
+    // return the constructor
     return User;
 });
