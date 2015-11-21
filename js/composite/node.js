c894a39b9425583ee5118018adb082989075c8ec
Jose Antonio Sanchez <jasancheg@gmail.com>
2015-11-21 13:24:55 -0600

    Composite pattern

--- js/composite/node.js
+++ js/composite/node.js
@@ -31,27 +31,41 @@ define(function () {
 
     /**
      * Method to traverse up
-     * @return {[type]} [description]
+     * It does not need to received any arguments
      */
     Person.prototype.traverseUp = function () {
+        // first check if the current object has a parent object
         if (this.parent) {
             console.log(this.name + ' is the child of ' + this.parent.name);
+            // invoke the traverseUp method but of the parent
             this.parent.traverseUp();
         } else {
+            // if there are no parents, we know that this is the root node
             console.log(this.name + ' is the root node');
         }
     };
 
+    /**
+     * Method to traverse down
+     * It does not need to received any arguments
+     */
     Person.prototype.traverseDown = function () {
+        // check whether there are children
         if (this.children.length) {
+            // if there are we need to iterate through the childre collection
+            // we should also set the context for the forEach method so that 
+            // inside the handler this stil refers to the current person object
             this.children.forEach(function (child) {
                 console.log(this.name + ' is the parent of ' + child.name);
+                // invoke the traverseDown method on the child
                 child.traverseDown();
             }, this);
         } else {
+            // if there are no childs, we know that this is the leaf node
             console.log(this.name + ' is a leaf node');
         }
     };
 
+    // return the constructor
     return Person;
 });
