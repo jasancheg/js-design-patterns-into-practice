bc674a595fda5d8fe851823306cae52ccd8cd0fa
Jose Antonio Sanchez <jasancheg@gmail.com>
2015-11-21 10:40:29 -0600

    Decorator pattern

--- js/decorator/decorators/exec.js
+++ js/decorator/decorators/exec.js
@@ -1,7 +1,16 @@
 define(function () {
     'use strict';
 
+    /**
+     * exec decorator
+     * this module just return an object that has its own implementation of
+     * the getPermissions method, ready to be copied to the object being decorated
+     */
     return {
+        /**
+         * new definition to overwrite getPermissions 
+         * @return {[function]} [new getPermissions implementation]
+         */
         getPermissions: function () {
             return 'public:read,confidential:read'
         }
